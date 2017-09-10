de9dd795e260a340139c4955d82951bea923c71f
Stephen Skubik-Peplaski <codesmith@me.com>
2017-09-09 22:14:27 -0400

    got the coreml model working properly

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -32,14 +32,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(imagePicker, animated: true, completion: nil)
     }
     
+    // MARK: - UIImagePickerController delegate methods
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = userPickedImage
             guard let ciImage = CIImage(image: userPickedImage) else {
                 fatalError("Could not convert UIImage into CIImage")
             }
+            detect(image: ciImage)
         }
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Image detection
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Loading CoreML model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            self.navigationItem.title = classification?.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+
     }
     
 }
